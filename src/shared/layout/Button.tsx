428716c94f21f6e52b7eba658e41515d3d483676
fonsirs <fonsirs@gmail.com>
2024-12-12 19:25:38 +0100

    create company page | fix button types

--- src/shared/layout/Button.tsx
+++ src/shared/layout/Button.tsx
@@ -1,10 +1,14 @@
 import { HTMLProps } from 'react';
 import styled from 'styled-components';
 
+interface Props extends HTMLProps<HTMLButtonElement> {
+    type: 'button' | 'submit' | 'reset' | undefined;
+}
+
 const StyledButton = styled.button`
     border-radius: 4px;
     padding: 8px 12px;
 `;
 
-export const Button = (props: HTMLProps<HTMLButtonElement>) =>
+export const Button = (props: Props) =>
     <StyledButton {...props}>{props.children}</StyledButton>
