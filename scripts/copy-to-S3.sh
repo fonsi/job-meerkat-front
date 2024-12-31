0f02b7d2930b6cb93fbfd4c794ed6ff5852e2521
fonsirs <fonsirs@gmail.com>
2024-12-31 19:45:55 +0100

    add github action that triggers a daily deploy

--- scripts/copy-to-S3.sh
+++ scripts/copy-to-S3.sh
@@ -2,6 +2,8 @@
 
 source .env.local
 
-export AWS_PROFILE=$AWS_PROFILE
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_REGION=$AWS_REGION
 
 aws s3 sync ./out $WEB_BUCKET --exclude "*.DS_Store" --delete
