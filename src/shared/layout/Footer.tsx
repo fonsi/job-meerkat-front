f7a6863fbba01eea956cb3c642b55f75ea775c89
fonsirs <fonsirs@gmail.com>
2025-01-05 16:56:53 +0100

    add social networks to page footer

--- src/shared/layout/Footer.tsx
+++ src/shared/layout/Footer.tsx
@@ -2,23 +2,82 @@
 
 import Link from 'next/link';
 import styled from 'styled-components';
+import { X } from '../image/icons/X';
+import { Bluesky } from '../image/icons/Bluesky';
+import { Linkedin } from '../image/icons/Linkedin';
+import { Container } from './Container';
+import { Colors } from '../styles/constants';
 
 const StyledFooter = styled.footer`
     align-items: center;
+    border-top: 1px solid ${Colors.mediumGrey};
     font-size: 12px;
     display: flex;
     justify-content: center;
-    padding: 8px 0;
+    padding: 8px 4px;
     width: 100%;
+`;
+
+const StyledMadeBy = styled.div``;
+
+const StyledSocial = styled.div`
+    display: flex;
+    gap: 12px;
+
+    svg {
+        width: 24px;
+        height: 24px;
+    }
+`;
+
+const SocialLink = styled(Link)`
+    align-items: center;
+    display: flex;
+    transition: color 0.2s ease-in;
+
+    :hover {
+        color: ${Colors.lightGrey};
+    }
+`;
+
+const StyledContainer = styled(Container)`
+    align-items: center;
+    flex-direction: row;
+    justify-content: space-between;
 `;
 
 export const Footer = () => (
     <StyledFooter>
-        <span>
-            Made with love by{' '}
-            <Link target="_blank" href="https://twitter.com/FonsiRS">
-                @fonsirs
-            </Link>
-        </span>
+        <StyledContainer>
+            <StyledMadeBy>
+                Made with love by{' '}
+                <Link target="_blank" href="https://x.com/FonsiRS">
+                    @fonsirs
+                </Link>
+            </StyledMadeBy>
+            <StyledSocial>
+                <SocialLink
+                    aria-label="Follow Jobmeerkat at X"
+                    target="_blank"
+                    href="https://x.com/jobmeerkat"
+                >
+                    <X />
+                </SocialLink>
+                <SocialLink
+                    aria-label="Follow Jobmeerkat at Bluesky"
+                    target="_blank"
+                    href="https://bsky.app/profile/jobmeerkat.bsky.social"
+                >
+                    <Bluesky />
+                </SocialLink>
+                <SocialLink
+                    aria-label="Follow Jobmeerkat at Linkedin"
+                    target="_blank"
+                    href="https://www.linkedin.com/company/jobmeerkat"
+                >
+                    <Linkedin />
+                </SocialLink>
+            </StyledSocial>
+        </StyledContainer>
     </StyledFooter>
 );
